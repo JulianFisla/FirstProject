c970f9a234217803bd2d71f779fab0337a5f9e5d
Dammer831 <ethan.m.dam@gmail.com>
2022-02-19 14:59:17 -0500

    julian

--- src/demoproject/Main.java
+++ src/demoproject/Main.java
@@ -1,18 +1,49 @@
 package demoproject;
 
-import org.lwjgl.*;
-import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+import java.util.Scanner;
+import java.util. Timer;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+import java.awt.event.*;
+
 
 //Main
 public class Main {
     public static int points = 0;
+    public static int posx = 0;
+    public static int posy = 0;
     public static enum State{
         Menu,
         Game,
         Death,
     };
+    public static enum Direction{
+        Up,
+        Down,
+        Left,
+        Right,
+    };
+    public static  State state = State.Menu;
+    public static Direction direction = Direction.Right;
 
+    public void move(){
+        if (direction == Direction.Up){
+            posy+=10;
+        }
+        if (direction == Direction.Down){
+            posy-=10;
+        }
+        if (direction == Direction.Left){
+            posx-=10;
+        }
+        if (direction == Direction.Right){
+            posx+=10;
+        }
+    }
     public static void main(String[] args) {
-        System.out.print("skull");
     }
 }
