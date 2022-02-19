21aa7508cb0c0613d96708a252cf048404b3145f
fun-haver <adrian.dipaola05@gmail.com>
2022-02-19 15:21:59 -0500

    finally i did some ounce of work

--- src/demoproject/graphics.java
+++ src/demoproject/graphics.java
@@ -7,12 +7,19 @@ public class graphics extends JPanel {
     // Creating the JPanel
     public graphics() {
         // JPanel Default Settings
-        setPreferredSize(new Dimension(600, 480));
+        setPreferredSize(new Dimension(1280, 720));
         setBackground(new Color(0, 0, 0));
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        g.drawRect (250,250,80, 80);
+
+    }
+    public static void main(String[] args){
+        JFrame newFrame = new JFrame("Snake :)");
+        newFrame.pack();
+        newFrame.setVisible(true);
     }
 }
