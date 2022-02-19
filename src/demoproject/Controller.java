6ffa2cbb31b2a7141d0bb39f55474777c1c9e842
Julian Fisla <94996608+JulianFisla@users.noreply.github.com>
2022-02-19 12:33:19 -0500

    Initial commit

---
+++ src/demoproject/Controller.java
@@ -0,0 +1,11 @@
+package demoproject;
+
+public class Controller {
+
+    public static void main(String[] args){
+        int x = 10;
+        int y = 5;
+        System.out.print(x+y);
+    }
+
+}
---
+++ src/demoproject/Main.java
@@ -0,0 +1,8 @@
+package demoproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World");
+    }
+}
