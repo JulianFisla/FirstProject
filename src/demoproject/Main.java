89f326c0eb725c3b628de7fccbf23d276f7521c4
ChristianFisla <cfisla@itmatter.com>
2022-02-19 12:47:52 -0500

    christian's commit woohoo

--- src/demoproject/Main.java
+++ src/demoproject/Main.java
@@ -1,8 +1,16 @@
 package demoproject;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("EW");
+
+        System.out.println("EW");
+        int theChosenOne = Math.min(8, 5);
+
+        System.out.printf("The chosen number is %1.2d", theChosenOne);
+
+
     }
 }
