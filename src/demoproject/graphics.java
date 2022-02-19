6e6caa894ff752246568b33786eaf5bb9c5ce3a5
ChristianFisla <cfisla@itmatter.com>
2022-02-19 15:41:16 -0500

    should work

--- src/demoproject/graphics.java
+++ src/demoproject/graphics.java
@@ -2,7 +2,7 @@ package demoproject;
 
 public class graphics {
 
-
     public static void main(String[] args) {
+
     }
 }
--- src/demoproject/grid.java
+++ src/demoproject/grid.java
@@ -1,2 +1,24 @@
-package demoproject;public class grid {
+package demoproject;
+
+//Import statement
+import java.util.Random;
+
+public class grid {
+
+    public void appleSpawn() {
+        Random rand = new Random();
+
+        //Dimensions 1280 x 720 pixels
+        int upperboundX = 1281;
+        int upperboundY = 721;
+
+        //Generate random values
+        int appleSpawnX = rand.nextInt(upperboundX);
+        int appleSpawnY = rand.nextInt(upperboundY);
+
+        System.out.println(appleSpawnX + "," + appleSpawnY);
+
+    }
+
 }
+
